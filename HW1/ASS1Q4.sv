330680bee3a494f612720ed524b293f30c1983f0
Melika S <melikaihelas@gmail.com>
2019-01-07 14:21:26 -0800

    commit

---
+++ HW1/ASS1Q4.sv
@@ -0,0 +1,25 @@
+module test(one,two,three,four,five,six,seven,eight,
+,key,out);
+
+input [31:0] one,two,three,four,five,six,seven,eight;
+output reg [31:0] out;
+reg [31:0] old_out;
+input [2:0] key;
+
+assign old_out = out;
+
+always@(*) begin
+
+if (key == 3'b000) out = one;
+else if (key == 3'b001) out = two;
+else if (key == 3'b010) out = three;
+else if (key == 3'b011) out = four;
+else if (key == 3'b100) out = five;
+else if (key == 3'b101) out = six;
+else if (key == 3'b110) out = seven;
+else if (key == 3'b111) out = eight;
+else out = old_out;
+
+
+end
+endmodule
---
+++ HW1/ASS1Q4_TB.sv
@@ -0,0 +1,32 @@
+module test_tb1();
+
+
+
+
+
+reg [4:0] one,two,three,four,five,six,seven,eight;
+wire [4:0] out;
+reg [2:0] key;
+
+test test(.one(one),.two(two),.three(three),.four(four),.five(five),.six(six),.seven(seven),.eight(eight),
+.key(key),.out(out));
+
+
+initial begin
+
+key = 3'b001;
+
+one = 5'b00001;
+two = 5'b01001;
+three = 5'b01001;
+four = 5'b01010;
+five = 5'b00101;
+six = 5'b00101;
+seven = 5'b01110;
+eight = 5'b01011;
+#10;
+
+
+end
+
+endmodule
