5b603a47ef83965247578efa6979ba533c0ca5b5
Melika S <melikaihelas@gmail.com>
2019-01-15 11:46:43 -0800

    LED control in prog

--- lab1_template_de1soc/LED_CONT.v
+++ lab1_template_de1soc/LED_CONT.v
@@ -1,12 +1,12 @@
-module LED_CONT ( LED, clk)
+module LED_CONT( LEDS, clk);
 
 
 input clk; // clock_50
-output [7:0] LED;
+output [7:0] LEDS;
 
 logic [31:0] count;
 
-enum { start,LED0, LED1,LED2,LED3,LED4,LED5,LED6,LED7,LED8} states;
+enum { start,LEDS0, LEDS1,LEDS2,LEDS3,LEDS4,LEDS5,LEDS6,LEDS7,LEDS8} state;
 
 
 always @(posedge clk) begin
@@ -19,141 +19,140 @@ case(state)
 
 start: begin
 
-LED [7:0] <= 0 ;
+LEDS [7:0] <= 0 ;
 count<= 0;
 
 end
 
 
-LED0: begin
+LEDS0: begin
 
-	if ( count >= 25000000 )
+	if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[0]<= 0;
-		state<=LED1;
+		LEDS[0]<= 0;
+		state<=LEDS1;  end
 		
 	else begin
 	count<= count +1;
-	LED[0] <= 1;
-	state<= LED0;
+	LEDS[0] <= 1;
+	state<= LEDS0;
 	end
 
 end
 
 
-LED1: begin 
+LEDS1: begin 
 
-if ( count >= 25000000 )
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[1]<= 0;
-		state<=LED2;
+		LEDS[1]<= 0;
+		state<=LEDS2; end
 		
 	else begin
 	count<= count +1;
-	LED[1] <= 1;
-	state<= LED1;
+	LEDS[1] <= 1;
+	state<= LEDS1;
 	end
 
 
 end
 
-LED2: begin 
+LEDS2: begin 
 
-if ( count >= 25000000 )
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[2]<= 0;
-		state<=LED3;
+		LEDS[2]<= 0;
+		state<=LEDS3; end
 		
 	else begin
 	count<= count +1;
-	LED[2] <= 1;
-	state<= LED2;
+	LEDS[2] <= 1;
+	state<= LEDS2;
 	end
 
 
 end
 
 
-LED3: begin
+LEDS3: begin
 
-if ( count >= 25000000 )
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[3]<= 0;
-		state<=LED4;
+		LEDS[3]<= 0;
+		state<=LEDS4; end
 		
 	else begin
 	count<= count +1;
-	LED[3] <= 1;
-	state<= LED3;
-	end
-
-
-
-end
-
-LED4: begin
-
-if ( count >= 25000000 )
-		count <= 0;
-		LED[4]<= 0;
-		state<=LED5;
-		
-	else begin
-	count<= count +1;
-	LED[4] <= 1;
-	state<= LED4;
+	LEDS[3] <= 1;
+	state<= LEDS3;
 	end
 
 
 
 end
 
-LED5: begin
+LEDS4: begin
 
-
-if ( count >= 25000000 )
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[5]<= 0;
-		state<=LED6;
-		
+		LEDS[4]<= 0;
+		state<=LEDS5; end
 	else begin
 	count<= count +1;
-	LED[5] <= 1;
-	state<= LED5;
+	LEDS[4] <= 1;
+	state<= LEDS4;
 	end
 
 
 
 end
 
-LED6: begin
+LEDS5: begin
 
-if ( count >= 25000000 )
+
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[6]<= 0;
-		state<=LED7;
+		LEDS[5]<= 0;
+		state<=LEDS6; end
 		
 	else begin
 	count<= count +1;
-	LED[6] <= 1;
-	state<= LED6;
+	LEDS[5] <= 1;
+	state<= LEDS5;
 	end
 
 
 
 end
 
-LED7: begin
+LEDS6: begin
 
-if ( count >= 25000000 )
+if ( count >= 25000000 ) begin
 		count <= 0;
-		LED[7]<= 0;
-		state<=LED0;
+		LEDS[6]<= 0;
+		state<=LEDS7; end
 		
 	else begin
 	count<= count +1;
-	LED[7] <= 1;
-	state<= LED7;
+	LEDS[6] <= 1;
+	state<= LEDS6;
+	end
+
+
+
+end
+
+LEDS7: begin
+
+if ( count >= 25000000 ) begin
+		count <= 0;
+		LEDS[7]<= 0;
+		state<=LEDS0; end
+		
+	else begin
+	count<= count +1;
+	LEDS[7] <= 1;
+	state<= LEDS7;
 	end
 
 
@@ -162,7 +161,7 @@ end
 
 default: state<= start;
 
-
+endcase
 end 
 
 
