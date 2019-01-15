80fb0045135bbcd555a8ff3697992a8fc480b8ac
Melika S <melikaihelas@gmail.com>
2019-01-15 11:16:08 -0800

    statemachin complete
    ready for test

--- lab1_template_de1soc/LED_CONT.v
+++ lab1_template_de1soc/LED_CONT.v
@@ -6,7 +6,7 @@ output [7:0] LED;
 
 logic [31:0] count;
 
-enum { start, finish , LED1,LED2,LED3,LED4,LED5,LED6,LED7,LED8} states;
+enum { start,LED0, LED1,LED2,LED3,LED4,LED5,LED6,LED7,LED8} states;
 
 
 always @(posedge clk) begin
@@ -15,6 +15,14 @@ always @(posedge clk) begin
 
 
 case(state)
+
+
+start: begin
+
+LED [7:0] <= 0 ;
+count<= 0;
+
+end
 
 
 LED0: begin
@@ -68,7 +76,16 @@ end
 
 LED3: begin
 
-
+if ( count >= 25000000 )
+		count <= 0;
+		LED[3]<= 0;
+		state<=LED4;
+		
+	else begin
+	count<= count +1;
+	LED[3] <= 1;
+	state<= LED3;
+	end
 
 
 
@@ -76,26 +93,74 @@ end
 
 LED4: begin
 
+if ( count >= 25000000 )
+		count <= 0;
+		LED[4]<= 0;
+		state<=LED5;
+		
+	else begin
+	count<= count +1;
+	LED[4] <= 1;
+	state<= LED4;
+	end
+
+
+
 end
 
 LED5: begin
 
+
+if ( count >= 25000000 )
+		count <= 0;
+		LED[5]<= 0;
+		state<=LED6;
+		
+	else begin
+	count<= count +1;
+	LED[5] <= 1;
+	state<= LED5;
+	end
+
+
+
 end
 
 LED6: begin
+
+if ( count >= 25000000 )
+		count <= 0;
+		LED[6]<= 0;
+		state<=LED7;
+		
+	else begin
+	count<= count +1;
+	LED[6] <= 1;
+	state<= LED6;
+	end
+
 
 
 end
 
 LED7: begin
 
+if ( count >= 25000000 )
+		count <= 0;
+		LED[7]<= 0;
+		state<=LED0;
+		
+	else begin
+	count<= count +1;
+	LED[7] <= 1;
+	state<= LED7;
+	end
+
+
 end
 
-LED8: begin
 
-end
-
-default: state<= start ;
+default: state<= start;
 
 
 end 
