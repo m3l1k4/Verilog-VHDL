7cea0d53a4ba31ce3faf1a76088371fb3c27fbaa
Melika S <melikaihelas@gmail.com>
2019-03-11 00:02:10 -0700

    working on cracker

--- LAB#4/template_de1soc/key_search.sv
+++ LAB#4/template_de1soc/key_search.sv
@@ -1,4 +1,24 @@
 module key_search();
 
+integer i;
+integer key;
+
+integer max_key = 24'b001111111111111111111111;
+
+
+
+always@() begin
+
+
+if ( key< max_key) begin
+
+
+	key <= key +1 ;
+
+
+
+end
+
+
 
 endmodule
